2809322952b1af082fadc46d2185adbc7773da84
Myst_lab1_JoseJavierGomez <42441935+Javier9696@users.noreply.github.com>
2018-11-08 23:52:40 -0600

    Add files via upload

---
+++ PROU.R
@@ -0,0 +1,96 @@
+# -- Borrar todos los elementos del environment
+rm(list=ls())
+mdir <- getwd()
+
+# -- Establecer el sistema de medicion de la computadora
+Sys.setlocale(category = "LC_ALL", locale = "")
+
+# -- Huso horario
+Sys.setenv(tz="America/Monterrey", TZ="America/Monterrey")
+options(tz="America/Monterrey", TZ="America/Monterrey")
+
+# -- Cargar y/o instalar en automatico paquetes a utilizar -- #
+
+pkg <- c("base","downloader","dplyr","fBasics","forecast","grid",
+         "gridExtra","httr","jsonlite","lmtest","lubridate","moments",
+         "matrixStats", "PerformanceAnalytics","plyr","quantmod",
+         "reshape2","RCurl","RMySQL", "stats","scales","tseries",
+         "TTR","TSA","XML","xts","zoo")
+
+inst <- pkg %in% installed.packages()
+if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
+instpackages <- lapply(pkg, library, character.only=TRUE)
+
+# -- Cargar archivos desde GitHub -- #
+
+RawGitHub <- "https://raw.githubusercontent.com/IFFranciscoME/"
+ROandaAPI <- paste(RawGitHub,"ROandaAPI/master/ROandaAPI.R",sep="")
+downloader::source_url(ROandaAPI,prompt=FALSE,quiet=TRUE)
+
+# -- Parametros para usar API-OANDA
+
+# Tipo de cuenta practice/live
+OA_At <- "practice"
+# ID de cuenta
+OA_Ai <- 1742531
+# Token para llamadas a API
+OA_Ak <- "ada4a61b0d5bc0e5939365e01450b614-4121f84f01ad78942c46fc3ac777baa6" 
+# Hora a la que se considera "Fin del dia"
+OA_Da <- 17
+# Uso horario
+OA_Ta <- "America/Mexico_City"
+# Instrumento
+OA_In <- "USD_MXN"
+# Granularidad o periodicidad de los precios H4 = Cada 4 horas
+OA_Pr <- "H4"
+# Multiplicador de precios para convertir a PIPS
+MultPip_MT1 <- 10000
+
+Precios_Oanda <- HisPrices(AccountType = OA_At, Granularity = OA_Pr,
+                           DayAlign = OA_Da, TimeAlign = OA_Ta, Token = OA_Ak,
+                           Instrument = OA_In, 
+                           Start = NULL, End = NULL, Count = 900)
+suppressMessages(library(plotly)) # Graficas interactivas
+Datos <- list()
+Datos <- Precios_Oanda
+suppressMessages(library(TTR)) # ANALISIS TEC
+Datos$TimeStamp <- as.character(Datos$TimeStamp)
+
+
+Datos$TimeStamp <- as.Date(Datos$TimeStamp)
+Historico <- data.frame("Date"= Datos[,1],
+                        "Precio"= Datos[,5])
+X <- EMA(Historico[,2],n=10, wilder = FALSE, ratio = NULL)
+y <- BBands(Historico[,2], n=10, EMA, sd=2)
+
+
+Historico <- data.frame("Date"= Datos[,1],
+                        "Precio"= Datos[,5],
+                        "EMA"= X,
+                        "BBd"= y[,1],
+                        "BBu"=y[,3])
+
+
+plot_ly(Historico) %>%
+  
+  add_trace(x = Historico[,1], y = ~round(Historico[,2]), type = 'scatter', mode = 'lines', name = 'Precio',
+            
+            line = list(color = 'red'), hoverinfo = "text", text = ~paste('Precio',round(Historico[,2]))) %>%
+  
+  add_trace(x = Historico[,1], y = ~round(Historico[,3]), type = 'scatter', mode = 'lines', name = 'EMA',
+            
+            line = list(color = 'blue'), hoverinfo = "text", text = ~paste('EMA',round(Historico[,3])))  %>%
+  add_trace(x = Historico[,1], y = ~round(Historico[,4]), type = 'scatter', mode = 'lines', name = 'BB',
+            
+            line = list(color = 'orange'), hoverinfo = "text", text = ~paste('BBd',round(Historico[,4])))  %>%
+  add_trace(x = Historico[,1], y = ~round(Historico[,5]), type = 'scatter', mode = 'lines', name = 'BB',
+            
+            line = list(color = 'black'), hoverinfo = "text", text = ~paste('BBu',round(Historico[,5])))  %>%
+  
+  layout(title = "3 Portafolios distintos objetivos",
+         
+         xaxis = list(title = "Fechas", showgrid = T),
+         
+         yaxis = list(title = "Precios"), 
+         
+         legend = list(orientation = 'h', y = -0.25, x = -0.25))
