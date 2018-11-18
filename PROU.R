d727caa9ffbe9da00e26a4c382877e5e30391fc8
Myst_lab1_JoseJavierGomez <42441935+Javier9696@users.noreply.github.com>
2018-11-18 12:42:15 -0600

    Código  para entrega 1
    El código hace gráficas con candleStick, la gráfica de precios con SMA,EMA Y BB bands.
    También según el candleStick  dice si es Bearish & Bullish  Engulfing, Bearish & Bullish  Harami y  hace un histograma de como se presentaron.
    Otra cosa que también ya hace ,  es analizar  la media móvil simple de 10 y 100 días, así como también  la Media exponencial de 10 días  y  gráfica  un histograma de la frecuencia en la que se presentó cada  uno.

--- PROU.R
+++ PROU.R
@@ -1,3 +1,4 @@
+# -- Borrar todos los elementos del environment
 rm(list=ls())
 mdir <- getwd()
 
@@ -12,7 +13,7 @@ options(tz="America/Monterrey", TZ="America/Monterrey")
 
 pkg <- c("base","downloader","dplyr","fBasics","forecast","grid",
          "gridExtra","httr","jsonlite","lmtest","lubridate","moments",
-         "matrixStats", "PerformanceAnalytics","plyr","quantmod",
+         "matrixStats", "quantmod","PerformanceAnalytics","plyr","quantmod",
          "reshape2","RCurl","RMySQL", "stats","scales","tseries",
          "TTR","TSA","XML","xts","zoo")
 
@@ -48,8 +49,9 @@ MultPip_MT1 <- 10000
 Precios_Oanda <- HisPrices(AccountType = OA_At, Granularity = OA_Pr,
                            DayAlign = OA_Da, TimeAlign = OA_Ta, Token = OA_Ak,
                            Instrument = OA_In, 
-                           Start = NULL, End = NULL, Count = 900)
+                           Start = NULL, End = NULL, Count = 500)
 suppressMessages(library(plotly)) # Graficas interactivas
+suppressMessages(library(quantmod)) # Graficas interactivas
 Datos <- list()
 Datos <- Precios_Oanda
 suppressMessages(library(TTR)) # ANALISIS TEC
@@ -57,6 +59,137 @@ Datos$TimeStamp <- as.character(Datos$TimeStamp)
 
 
 Datos$TimeStamp <- as.Date(Datos$TimeStamp)
+Historico <- data.frame("Date"= Datos[,1],
+                        "Precioopen"= Datos[,2],
+                        "Preciohigh"= Datos[,3],
+                        "Preciolow"= Datos[,4],
+                        "Precioclose"= Datos[,5])
+SM <- SMA(Historico[,2],n=10, wilder = FALSE, ratio = NULL)
+Z <- SMA(Historico[,2],n=100, wilder = FALSE, ratio = NULL)
+x_Ema <-EMA(Historico[,2],n=10, wilder = FALSE, ratio = NULL)
+y <- BBands(Historico[,2], n=10, EMA, sd=2)
+
+Historico <- data.frame("Date"= Datos[,1],
+                        "Precioopen"= Datos[,2],
+                        "Preciohigh"= Datos[,3],
+                        "Preciolow"= Datos[,4],
+                        "Close"= Datos[,5],
+                        "SMA_10" = SM,
+                        "SMA_100" = Z,
+                        "EMA_10 " = x_Ema,
+                        "BBd"= y[,1],
+                        "BBu"=y[,3],
+                        "Mov_10" = 0,
+                        "Mov_100" = 0,
+                        "Mov_E" = 0)
+                        
+                        
+                        
+                        
+X <- xts(Historico[,2:5],order.by= Historico[,1], frequency = NULL, unique = TRUE)
+is.xts(X)
+
+chartSeries(X[,1:4],type = c("candlesticks"), subset = NULL, name = "Candlestick", time.scale= NULL,log.scale = FALSE,
+           TA = 'addVo()',
+          TAsep=';',
+         line.type = "l",
+        bar.type = "ohlc",
+       theme = chartTheme("white"),
+      layout = NA,
+     major.ticks='auto', minor.ticks=TRUE,
+    yrange=NULL,
+   plot=TRUE,
+  color.vol = TRUE, multi.col = FALSE)
+candleChart(X[,1:4],subset = NULL, type = "candlesticks", show.grid = TRUE, theme = 'white')
+#add_BBands(n=20,maType="SMA",sd=2)
+
+chartSeries(X,
+            type = c("line"), 
+            subset = NULL,
+            show.grid = TRUE, 
+            name = "Precios",
+            time.scale = NULL,
+            log.scale = FALSE,
+            TA = 'addVo()',
+            TAsep=';',
+            line.type = "l",
+            bar.type = "ohlc",
+            theme = chartTheme("white"),
+            layout = NA,
+            major.ticks='auto', minor.ticks=TRUE,
+            yrange=NULL,
+            plot=TRUE)
+addSMA(n = 10, on = 1, with.col = Cl, overlay = TRUE, col = "brown")
+addSMA(n = 100, on = 1, with.col = Cl, overlay = TRUE, col = "orange")
+addEMA(n = 10, wilder = FALSE, ratio=NULL, on = 1,
+       with.col = Cl, overlay = TRUE, col = "blue")
+addBBands(n = 20, sd = 2, ma = "SMA", draw = 'bands', on = -1)
+
+
+#Analisis de los estadisticos
+for ( i in 10:length(Historico$Date)){
+  
+  if (Historico$Precioopen[i] > Historico$SMA_10[i]){
+    Historico$Mov_10[i]<-1 #compra    
+    
+  }#fin primer if 
+  else if (Historico$Precioopen[i] < Historico$SMA_10[i]){
+    Historico$Mov_10[i] <-2   
+    
+  } else
+  { Historico$Mov_10[i]<-0
+  
+  }
+  #mantiene
+  if (Historico$Precioopen[i] > Historico$EMA_10[i]){
+    Historico$Mov_E[i]<-1 #compra    
+    
+  }#fin primer if 
+  else if (Historico$Precioopen[i] < Historico$EMA_10[i]){
+    Historico$Mov_E[i] <-2   
+    
+  } else
+  { Historico$Mov_E[i]<-0
+  
+  }
+  
+}
+#evaluacion MA 100
+for ( i in 100:length(Historico$Date)){
+  
+  if (Historico$Precioopen[i] > Historico$SMA_100[i]){
+    Historico$Mov_100[i]<-1 #compra    
+    
+  }#fin primer if 
+  else if (Historico$Precioopen[i] < Historico$SMA_100[i]){
+    Historico$Mov_100[i] <-2    
+  } else
+  { Historico$Mov_100[i]<-0} #mantiene
+  
+}
+
+#MA 10
+hist(Historico$Mov_10,
+     main="Frecuencia de Movimientos contra SMA de 10",
+     xlab="Patrones:  0.- Mantiene, 1.-Compra, 2.-Venta",
+     col="Green",
+     freq=FALSE
+)
+
+#MA 100
+hist(Historico$Mov_100,
+     main="Frecuencia de Movimientos contra SMA de 100",
+     xlab="Patrones:  0.- Mantiene, 1.-Compra, 2.-Venta",
+     col="Blue",
+     freq=FALSE
+)
+#EMA
+hist(Historico$Mov_E,
+     main="Frecuencia de Movimientos contra EMA de 100",
+     xlab="Patrones:  0.- Mantiene, 1.-Compra, 2.-Venta",
+     col="Green",
+     freq=FALSE
+)
 
 #Patrones CandleSticks
 aux <-data.frame("Date" = Datos[,1],
@@ -64,7 +197,15 @@ aux <-data.frame("Date" = Datos[,1],
                  "Precio_Cierre" = Datos$Close,
                  "Bull_Bear" = NA , 
                  "Amplitud" = 0,
-                 "Pattern" = NA)
+                 "Pattern" = NA,
+                 "Number" = 0)
+#numbers
+#bearish enfulfing 1
+#bullish enfulfing 2
+# bearish Harami 3
+# bullish Harami 4
+
+
 aux$Bull_Bear[1] <- "Bull"
 aux$Amplitud[1] <- abs(aux$Precio_Cierre[1] - aux$Precio_Apertura[1])
 
@@ -84,92 +225,57 @@ for (i in 2:length(Datos$Open)){
   if( aux$Amplitud[i-1] < aux$Amplitud[i]){
     if (aux$Bull_Bear[i-1] == "Bull" & aux$Bull_Bear[i] == "Bear") {
       aux$Pattern[i] <- "Bearish Engulfing" 
+      aux$Number[i]<-1
       
     }
     else if (aux$Bull_Bear[i-1] == "Bear" & aux$Bull_Bear[i] == "Bull"){
-      aux$Pattern[i] <- "Bullish Engulfing" 
+      aux$Pattern[i] <- "Bullish Engulfing"
+      aux$Number[i] <- 2
     }
     if (aux$Bull_Bear[i-1] == "Bull" & aux$Bull_Bear[i] == "Bull") {
       aux$Pattern[i] <- "Bullish"
+      aux$Number[i] <- 2
       
     }
     else if (aux$Bull_Bear[i-1] == "Bear" & aux$Bull_Bear[i] == "Bear"){
       aux$Pattern[i] <- "Bearish"
+      aux$Number[i]<-1
     }
   }
   
   else if(aux$Amplitud[i-1] > aux$Amplitud[i]){
     if (aux$Bull_Bear[i-1] == "Bull" & aux$Bull_Bear[i] == "Bear") {
-      aux$Pattern[i] <- "Bearish Harami" 
+      aux$Pattern[i] <- "Bearish Harami"
+      aux$Number[i]<-3
       
     }
     else if(aux$Bull_Bear[i-1] == "Bear" & aux$Bull_Bear[i] == "Bull") {
       #else if(aux$Bull_Bear[i-1] == "Bear" & aux$Bull_Bear[i] == "Bull")  {
       aux$Pattern[i] <- "Bullish Harami" 
+      aux$Number[i] <- 4
     }
     if (aux$Bull_Bear[i-1] == "Bull" & aux$Bull_Bear[i] == "Bull") {
       aux$Pattern[i] <- "Bullish"
+      aux$Number[i] <- 4
       
     }
     else if(aux$Bull_Bear[i-1] == "Bear" & aux$Bull_Bear[i] == "Bear") {
       
       aux$Pattern[i] <- "Bearish"
+      aux$Number[i]<-3
     }
     
   }
   
 }  # fin for
+bearish_egulfing <- length(which(aux$Number == 1))
+bullish_egulfing <- length(which(aux$Number == 2))
+bearish_harami <- length(which(aux$Number == 3))
+bearish_harami <- length(which(aux$Number == 4))
+hist(aux$Number,
+     main="Frecuencia de Patrones en CandleStick",
+     xlab="Patrones: 1.-Bearish Egulfing, 2.-Bullish Egulfing, 3.-Bearish Harami, 4.-Bearish Harami",
+     col="darkmagenta",
+     freq=TRUE
+)
 
-
-
-
-#Grafica EMA Y BBBands
-
-Historico <- data.frame("Date"= Datos[,1],
-                        "Precio"= Datos[,5])
-X <- EMA(Historico[,2],n=10, wilder = FALSE, ratio = NULL)
-y <- BBands(Historico[,2], n=10, EMA, sd=2)
-
-Historico <- data.frame("Date"= Datos[,1],
-                        "Precio"= Datos[,5],
-                        "EMA"= X,
-                        "BBd"= y[,1],
-                        "BBu"=y[,3],
-                        "Open" = Datos$Open,
-                        "Close" = Datos$Close,
-                        "High" = Datos$High,
-                        "Low" = Datos$Low)
-
-
-plot_ly(Historico) %>%
-  
-  add_trace(x = Historico[,1], y = ~round(Historico[,2]), type = 'scatter', mode = 'lines', name = 'Precio',
-            
-            line = list(color = 'red'), hoverinfo = "text", text = ~paste('Precio',round(Historico[,2]))) %>%
-  
-  add_trace(x = Historico[,1], y = ~round(Historico[,3]), type = 'scatter', mode = 'lines', name = 'EMA',
-            
-            line = list(color = 'blue'), hoverinfo = "text", text = ~paste('EMA',round(Historico[,3])))  %>%
-  add_trace(x = Historico[,1], y = ~round(Historico[,4]), type = 'scatter', mode = 'lines', name = 'BB',
-            
-            line = list(color = 'orange'), hoverinfo = "text", text = ~paste('BBd',round(Historico[,4])))  %>%
-  add_trace(x = Historico[,1], y = ~round(Historico[,5]), type = 'scatter', mode = 'lines', name = 'BB',
-            
-            line = list(color = 'black'), hoverinfo = "text", text = ~paste('BBu',round(Historico[,5])))  %>%
-  
-  layout(title = "3 Portafolios distintos objetivos",
-         
-         xaxis = list(title = "Fechas", showgrid = T),
-         
-         yaxis = list(title = "Precios"), 
-         
-         legend = list(orientation = 'h', y = -0.25, x = -0.25))
-
-#grafica candleStick 
-
-plot_ly(x =Historico$Date, open =Historico$Open, high = Historico$High, low = Historico$Low, close = Historico$Close, type = "candlestick")%>%
-  layout(title = "Gráfica de barras USD/MXN",
-         
-         xaxis = list(title = "Fechas", showgrid = T),
-         
-         yaxis = list(title = "Precios"))
