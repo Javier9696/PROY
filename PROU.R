f3f7f0ea803398376658abb0723d0069aa7e5432
Myst_lab1_JoseJavierGomez <42441935+Javier9696@users.noreply.github.com>
2018-11-26 17:39:24 -0600

    Proyecto al 26 de Noviembre

--- PROU.R
+++ PROU.R
@@ -1,8 +1,8 @@
 # -- Borrar todos los elementos del environment
 rm(list=ls())
 mdir <- getwd()
-
 # -- Establecer el sistema de medicion de la computadora
+
 Sys.setlocale(category = "LC_ALL", locale = "")
 
 # -- Huso horario
@@ -13,9 +13,9 @@ options(tz="America/Monterrey", TZ="America/Monterrey")
 
 pkg <- c("base","downloader","dplyr","fBasics","forecast","grid",
          "gridExtra","httr","jsonlite","lmtest","lubridate","moments",
-         "matrixStats", "quantmod","PerformanceAnalytics","plyr","quantmod",
-         "reshape2","RCurl","RMySQL", "stats","scales","tseries",
-         "TTR","TSA","XML","xts","zoo")
+         "matrixStats", "PerformanceAnalytics","plyr","quantmod",
+         "reshape2","RCurl","RMySQL", "stats","scales","tseries","stats",
+         "TTR","TSA","XML","xts","zoo","TSA")
 
 inst <- pkg %in% installed.packages()
 if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
@@ -68,6 +68,7 @@ SM <- SMA(Historico[,2],n=10, wilder = FALSE, ratio = NULL)
 Z <- SMA(Historico[,2],n=100, wilder = FALSE, ratio = NULL)
 x_Ema <-EMA(Historico[,2],n=10, wilder = FALSE, ratio = NULL)
 y <- BBands(Historico[,2], n=10, EMA, sd=2)
+macd <-MACD(Historico[,2], nFast = 10, nSlow = 28, nSig = 9, percent = TRUE) # Macd
 
 Historico <- data.frame("Date"= Datos[,1],
                         "Precioopen"= Datos[,2],
@@ -83,7 +84,7 @@ Historico <- data.frame("Date"= Datos[,1],
                         "Mov_100" = 0,
                         "Mov_E" = 0)
                         
-                        
+#---------------------------------------SMA 10, SMA 100, EMA 10                        
                         
                         
 X <- xts(Historico[,2:5],order.by= Historico[,1], frequency = NULL, unique = TRUE)
@@ -129,11 +130,11 @@ addBBands(n = 20, sd = 2, ma = "SMA", draw = 'bands', on = -1)
 #Analisis de los estadisticos
 for ( i in 10:length(Historico$Date)){
   
-  if (Historico$Precioopen[i] > Historico$SMA_10[i]){
+  if (Historico$Close[i] > Historico$SMA_10[i]){
     Historico$Mov_10[i]<-1 #compra    
     
   }#fin primer if 
-  else if (Historico$Precioopen[i] < Historico$SMA_10[i]){
+  else if (Historico$Close[i] < Historico$SMA_10[i]){
     Historico$Mov_10[i] <-2   
     
   } else
@@ -141,11 +142,11 @@ for ( i in 10:length(Historico$Date)){
   
   }
   #mantiene
-  if (Historico$Precioopen[i] > Historico$EMA_10[i]){
+  if (Historico$Close[i] > Historico$EMA_10[i]){
     Historico$Mov_E[i]<-1 #compra    
     
   }#fin primer if 
-  else if (Historico$Precioopen[i] < Historico$EMA_10[i]){
+  else if (Historico$Close[i] < Historico$EMA_10[i]){
     Historico$Mov_E[i] <-2   
     
   } else
@@ -157,11 +158,11 @@ for ( i in 10:length(Historico$Date)){
 #evaluacion MA 100
 for ( i in 100:length(Historico$Date)){
   
-  if (Historico$Precioopen[i] > Historico$SMA_100[i]){
+  if (Historico$Close[i] > Historico$SMA_100[i]){
     Historico$Mov_100[i]<-1 #compra    
     
   }#fin primer if 
-  else if (Historico$Precioopen[i] < Historico$SMA_100[i]){
+  else if (Historico$Close[i] < Historico$SMA_100[i]){
     Historico$Mov_100[i] <-2    
   } else
   { Historico$Mov_100[i]<-0} #mantiene
@@ -191,7 +192,7 @@ hist(Historico$Mov_E,
      freq=FALSE
 )
 
-#Patrones CandleSticks
+#----------------------------------------------------Patrones CandleSticks
 aux <-data.frame("Date" = Datos[,1],
                  "Precio_Apertura" = Datos$Open,
                  "Precio_Cierre" = Datos$Close,
@@ -271,11 +272,75 @@ for (i in 2:length(Datos$Open)){
 bearish_egulfing <- length(which(aux$Number == 1))
 bullish_egulfing <- length(which(aux$Number == 2))
 bearish_harami <- length(which(aux$Number == 3))
-bearish_harami <- length(which(aux$Number == 4))
+bullish_harami <- length(which(aux$Number == 4))
 hist(aux$Number,
      main="Frecuencia de Patrones en CandleStick",
-     xlab="Patrones: 1.-Bearish Egulfing, 2.-Bullish Egulfing, 3.-Bearish Harami, 4.-Bearish Harami",
+     xlab="Patrones: 1.-Bearish Egulfing, 2.-Bullish Egulfing, 3.-Bearish Harami, 4.-Bullish Harami",
      col="darkmagenta",
      freq=TRUE
 )
+
+###---- Bollinger Bands
+compra <- 0
+venta <- 0
+mantiene <- 0
+for (i in 10:length(Historico$Date)){
+
+  if(Historico$Close[i]< Historico$BBd[i]){
+    compra<- compra +1
+  }
+  else if (Historico$Close[i]> Historico$BBu[i]){
+    venta <- venta +1
+  }
+  else{
+    mantiene <- mantiene + 1
+  }
+    
+  
+} # fin for
+
+#---------------------MACD
+
+
+
+
+#graficaMACD
+chartSeries(X,
+            type = c("line"), 
+            subset = NULL,
+            show.grid = TRUE, 
+            name = "Precios",
+            time.scale = NULL,
+            log.scale = FALSE,
+            TA = 'addVo()',
+            TAsep=';',
+            line.type = "l",
+            bar.type = "ohlc",
+            theme = chartTheme("white"),
+            layout = NA,
+            major.ticks='auto', minor.ticks=TRUE,
+            yrange=NULL,
+            plot=TRUE)
+addMACD(fast = 10, slow = 28, signal = 9, type = "SMA")
+
+#----------------------------------BOX Jenkins
+ven_500 <- Historico$Close #todos  los valores 
+ven_150 <- Historico$Close[1:150] #primeros 150 valores
+ven_300 <- Historico$Close[151:300]
+ven_301 <- Historico$Close[301:500]
+
+
+Box <- c()
+Box[1]<- Box.test(ven_150, lag = 1, type = "Box-Pierce")$p.value
+Box[2]<- Box.test(ven_300, lag = 1, type = "Box-Pierce")$p.value
+Box[3]<- Box.test(ven_301, lag = 1, type = "Box-Pierce")$p.value
+Box[4]<- Box.test(ven_500, lag = 1, type = "Box-Pierce")$p.value
+
+
+#-----------------------------------Normalidad
+norm <- c()
+norm[1] <- ks.test(ven_150, "pnorm", 1, 2)$p.value
+norm[2] <- ks.test(ven_300, "pnorm", 1, 2)$p.value
+norm[3] <- ks.test(ven_301, "pnorm", 1, 2)$p.value
+norm[4] <- ks.test(ven_500, "pnorm", 1, 2)$p.value
 
